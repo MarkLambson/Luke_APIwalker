a3ea981126d863c0ccd0509a7b9d3d9f81790e40
MarkLambson <mj.lambson@yahoo.com>
2023-05-15 12:06:40 -0700

    added useNavigate

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,38 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
 
     const [resource, setResource] = useState('people');
     const [selectedId, setSelectedId] = useState(0);
 
+    //adding in useNavigate *best practice*
+    const navigate = useNavigate();
+
+    const search = (event) => {
+        event.preventDefault();
+        navigate(`/${resource}/${selectedId}`)
+    }
+
     return (
         <div>
             <h1>Welcome to Luke APIwalker!</h1>
             <br />
-            <label>Search for:</label>&nbsp;
-            <select name="resource" id="search-select" onChange={(event) => setResource(event.target.value)}>
-                <option value='people'>People</option>
-                <option value='planet'>Planets</option>
-            </select>&nbsp;&nbsp;&nbsp;
-            <label>ID: </label>&nbsp;
-            <input type='number' name="selectedId" id="search-id" onChange={(event) => setSelectedId(event.target.value)} />&nbsp;&nbsp;&nbsp;
-            <Link to={`/${resource}/${selectedId}`}>
+            {/* using a form for useNavigate */}
+            <form onSubmit={search}>
+                <label>Search for:</label>&nbsp;
+                <select name="resource" id="search-select" onChange={(event) => setResource(event.target.value)}>
+                    <option value='people'>People</option>
+                    <option value='planet'>Planets</option>
+                </select>&nbsp;&nbsp;&nbsp;
+                <label>ID: </label>&nbsp;
+                <input type='number' name="selectedId" id="search-id" onChange={(event) => setSelectedId(event.target.value)} />&nbsp;&nbsp;&nbsp;
+                {/* <Link to={`/${resource}/${selectedId}`}> */}
                 <button className='btn btn-outline-info'> Search </button>
-            </Link>
+                {/* </Link> */}
+            </form>
+            {/* using a form for useNavigate */}
             <hr />
         </div>
     );
